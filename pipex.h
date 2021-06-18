e8883cbb6ee1c3938bb355186c28ec65907e0c72
TH0RlN <rarias-p@student.42madrid.com>
2021-06-18 17:26:44 +0200

    .

--- pipex.h
+++ pipex.h
@@ -1,27 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rarias-p <rarias-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/06/17 17:25:19 by rarias-p          #+#    #+#             */
-/*   Updated: 2021/06/17 19:18:34 by rarias-p         ###   ########.fr       */
+/*   Created: 2021/06/17 17:58:29 by rarias-p          #+#    #+#             */
+/*   Updated: 2021/06/17 18:08:37 by rarias-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
+#ifndef PIPEX_H
+# define PIPEX_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
 
-int	main(int argc, char const **argv, char const **env)
-{
-	int		fd;
-	char	**splitted;
+/*                                 LIBFT                                      */
 
-	if (argc == 3)
-	{
-		fd = open(argv[1]);
-		dup2(fd, 0);
-		splitted = ft_split(argv[2], ' ');
-		execve("/usr/bin/grep", &splitted[1], env);
-	}
-}
+char	**ft_split		(char const *s, char c);
+
+#endif
---
+++ split.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   split.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rarias-p <rarias-p@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/17 17:58:14 by rarias-p          #+#    #+#             */
+/*   Updated: 2021/06/17 18:08:43 by rarias-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <pipex.h>
+
+static int	ft_countwords(char const *s, char c)
+{
+	int	i;
+	int	words;
+
+	i = 0;
+	words = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			i++;
+		else
+		{
+			words++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+	}
+	return (words);
+}
+
+static int	size_nextword(char const *s, char c, int i)
+{
+	int	counter;
+
+	while (s[i] == c && s[i])
+		i++;
+	counter = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (counter);
+		counter++;
+		i++;
+	}
+	return (counter);
+}
+
+static int	save_word(char *str, char const *s, char c, int i)
+{
+	int	j;
+
+	j = 0;
+	while (s[i] == c)
+		i++;
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			str[j] = '\0';
+			return (i);
+		}
+		str[j] = s[i];
+		i++;
+		j++;
+	}
+	str[j] = '\0';
+	return (i);
+}
+
+static char	**ft_split2(char const *s, char c, int max)
+{
+	int		j;
+	char	**tab;
+	int		i;
+
+	i = ft_countwords(s, c);
+	(tab = (char **)malloc(sizeof(char *) * (i + 1)));
+	tab[i] = NULL;
+	max = i;
+	i = 0;
+	j = 0;
+	while (j < max)
+	{
+		tab[j] = malloc(sizeof(char) * (size_nextword(s, c, i) + 1));
+		i = save_word(tab[j], s, c, i);
+		j++;
+	}
+	return (tab);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int	max;
+
+	if (s == NULL)
+		return (NULL);
+	max = 0;
+	return (ft_split2(s, c, max));
+}
